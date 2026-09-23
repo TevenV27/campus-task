ad3ccee8aa4565b673a9150df819b191228917a0
PaulaJ2006 <paula.bohorquez@correounivalle.edu.co>
2026-09-23 17:03:29 -0500

    Creacion de nuevas tareas

--- backend/src/tareas/tareas.service.ts
+++ backend/src/tareas/tareas.service.ts
@@ -7,6 +7,10 @@ export class TareasService {
     return [
       { id: 1, titulo: 'Leer la guía de la clase 2' },
       { id: 2, titulo: 'Preparar el entorno de desarrollo' },
+      { id: 3, titulo: 'Instalar dependencias' },
+      { id: 4, titulo: 'Crear el proyecto' },
+      { id: 5, titulo: 'Configurar el proyecto' },
+      { id: 6, titulo: 'Crear una nueva rama'}
     ];
   }
 }
