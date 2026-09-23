a7f8ea1e411a0e17ea646b3bc5f17d1783f6c02f
JMCRmeteorito <jose.castano.rojas@correounivalle.edu.co>
2026-09-23 17:39:46 -0500

    Corrigiendo nueva tarea

--- backend/src/tareas/tareas.service.ts
+++ backend/src/tareas/tareas.service.ts
@@ -7,7 +7,7 @@ export class TareasService {
     return [
       { id: 1, titulo: 'Leer la guía de la clase 2' },
       { id: 2, titulo: 'Preparar el entorno de desarrollo' },
-      { id: 3, titulo: 'Instalar dependencias del proyecto como nicolocaina' },
+      { id: 3, titulo: 'Instalar dependencias del proyecto como .nicolocaina' },
     ];
   }
 }
